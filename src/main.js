15722698d557f697b5384ea1923c0262c599b929
shinsihu10123 <b5h7s88sv7@privaterelay.appleid.com>
2026-08-01 21:32:18 +0900

    Load complete center schema guard and bust caches

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-const BUILD_ID = 'v0811-terrain-centers-hotfix-20260801'
+const BUILD_ID = 'v082-centers-contract-hotfix-20260801'
 const sourceParts = [
   './main.part1.js.txt',
   './main.part2.js.txt',
@@ -19,6 +19,7 @@ const sourceParts = [
   './main.part3l.js.txt',
   './main.part3m.js.txt',
   './main.part3n.js.txt',
+  './main.part3p.js.txt',
   './main.part3o.js.txt',
 ]
 
